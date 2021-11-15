feb91fed1bbf98038e070a1fbfb6508dacc96e97
Adnan Ahmed <adnanahmed.indian@gmail.com>
2021-11-15 19:21:50 +0530

    Made House APIs

--- Server/Server.js
+++ Server/Server.js
@@ -18,6 +18,11 @@ expressApp.use(express.urlencoded({extended: false}));
 
 //Adding routes
 expressApp.use("/user", require("./Routes/UserApi").router);
+expressApp.use("/house", require("./Routes/HouseApi").router);
+
+expressApp.get("/", (req,res)=> {
+    return res.status(200).send("Welcome to Community Support")
+})
 
 //Starting the express server
 expressApp.listen(5000, "0.0.0.0", () => console.log("Express Server started at port 5000"));
